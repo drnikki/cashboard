abcdd42729a108f921b4556d3f4ba1ddecca43fb
Nikki Stevens <nikkilstevens@gmail.com>
2013-11-11 17:16:44 -0500

    I DO NOT KNOW HOW TO USE GIT.

--- config_sample.js
+++ config_sample.js
@@ -1,4 +1,4 @@
-var config = {}
+var config = {};
 
 config.twitter = {};
 config.instagram = {};
@@ -12,18 +12,15 @@ config.twitter.user_name = process.env.TWITTER_USER || 'username';
 config.twitter.password =  process.env.TWITTER_PASSWORD || 'password';
 
 // GOOGLE ANALYTICS
+// this is the file they gave us
 config.google_analytics.json_path = 'client_secret_9999999.project.googleusercontent.com.json';
+// this is the data in the file
 config.google_analytics.web.auth_uri = 'https://accounts.google.com/o/oauth2/auth';
 config.google_analytics.web.client_secret = 'asdfasdfasdf';
-
-
-{"web":{
-	"auth_uri":"https://accounts.google.com/o/oauth2/auth",
-	"client_secret":"6rSZfsoMDbyfLefVXtnrJrWJ",
-"token_uri":"https://accounts.google.com/o/oauth2/token",
-"client_email":"1613113559@project.googleusercontent.com",
-"client_x509_cert_url":"https://www.googleapis.com/robot/v1/metadata/x509/1613113559@project.googleusercontent.com",
-"client_id":"1613113559.project.googleusercontent.com",
-"auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs"}}
+config.google_analytics.token_uri = 'https://accounts.google.com/o/oauth2/token';
+config.google_analytics.client_email = '9999999@project.googleusercontent.com';
+config.google_analytics.client_x509_cert_url = 'https://www.googleapis.com/robot/v1/metadata/x509/9999999@project.googleusercontent.com';
+config.google_analytics.client_id = '9999999.project.googleusercontent.com';
+config.google_analytics.auth_provider_x509_cert_url = 'https://www.googleapis.com/oauth2/v1/certs';
 
 module.exports = config;
