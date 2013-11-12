a6e6fcf581d072d6b33b08e93779be7aa4e73846
matthewlein <matthewlein@hotmail.com>
2013-11-12 15:19:57 -0600

    templates output

--- public/js/cashboard.js
+++ public/js/cashboard.js
@@ -5,3 +5,38 @@
 //
 // ========================================================================= //
 // ************************************************************************* //
+
+jQuery(document).ready(function($) {
+
+    var gaData;
+    var gaTemplate;
+
+    var getGaData = $.ajax({
+            url: 'data/googleanalytics'
+        })
+        .done(function(data){
+            gaData = data;
+            getGaTemplate();
+        });
+
+    function getGaTemplate() {
+        $.ajax({
+                url: 'js/templates/cashboard.html'
+            })
+            .done(function(data) {
+                gaTemplate = data;
+                renderGaTemplate();
+            });
+    }
+
+    function renderGaTemplate() {
+        var template = Hogan.compile(gaTemplate);
+        var rendered = template.render(gaData.metrics[0]);
+        console.log(gaData);
+        $('body').append(rendered);
+    }
+
+
+
+
+});
