874637c953743def1e2c60b3d9bfff07caa398a5
matthewlein <matthewlein@hotmail.com>
2013-11-12 14:17:13 -0600

    and now the routes are working as i imagined

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var routes = require('./routes');
+var data = require('./routes/data');
 
 var app = express();
 
@@ -42,32 +43,4 @@ app.get( '/', routes.index );
 
 // var config = require('config.js');
 
-app.get( '/data/googleanalytics', function(req, res) {
-
-
-    var GA = require('googleanalytics');
-    var util = require('util');
-    var GAconfig = {
-        "user" : 'test@yellowsmith.com',
-        "password": "testtest"
-    };
-    var ga = new GA.GA(GAconfig);
-
-    ga.login(function(err, token) {
-
-        var options = {
-            'ids': 'ga:77232304',
-            'start-date': '2013-10-11',
-            'end-date': '2013-11-30',
-            //'dimensions': 'ga:visitCount',
-            'metrics': 'ga:visitors, ga:percentNewVisits',
-            //'sort': '-ga:visitCount'
-        };
-
-        ga.get(options, function(err, entries) {
-            // its an array and I don't want that
-            res.json(entries[0]);
-        });
-    });
-
-});
+app.get( '/data/googleanalytics', data.data);
--- routes/data.js
+++ routes/data.js
@@ -1,8 +1,30 @@
 
 
 exports.data = function(req, res){
-    console.log('data');
-    res.render('data', {
-        title: 'Data'
+
+    var GA = require('googleanalytics');
+    var util = require('util');
+    var GAconfig = {
+        "user" : 'test@yellowsmith.com',
+        "password": "testtest"
+    };
+    var ga = new GA.GA(GAconfig);
+
+    ga.login(function(err, token) {
+
+        var options = {
+            'ids': 'ga:77232304',
+            'start-date': '2013-10-11',
+            'end-date': '2013-11-30',
+            //'dimensions': 'ga:visitCount',
+            'metrics': 'ga:visitors, ga:percentNewVisits',
+            //'sort': '-ga:visitCount'
+        };
+
+        ga.get(options, function(err, entries) {
+            // its an array and I don't want that
+            res.json(entries[0]);
+        });
     });
+
 };
