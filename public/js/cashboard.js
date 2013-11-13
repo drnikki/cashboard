95bb4614b9770ccce5fb08104dba03dd6838b925
matthewlein <matthewlein@hotmail.com>
2013-11-13 14:50:36 -0600

    makeGA function wrapper

--- public/js/cashboard.js
+++ public/js/cashboard.js
@@ -8,20 +8,29 @@
 
 jQuery(document).ready(function($) {
 
-    var getGaData = $.ajax({
+    function makeGoogle() {
+
+        var getGaData = $.ajax({
             url: 'data/googleanalytics'
         });
 
-    var getGaTemplate = $.ajax({
+        var getGaTemplate = $.ajax({
             url: 'js/templates/cashboard.html'
         });
 
-    $.when(getGaData, getGaTemplate)
-        .done(function (gaData, gaTemplate) {
-            var template = Hogan.compile(gaTemplate[0]);
-            var rendered = template.render(gaData[0].metrics[0]);
-            console.log(gaData);
-            $('body').append(rendered);
-        });
+        $.when(getGaData, getGaTemplate)
+            .done(function (gaData, gaTemplate) {
+                var template = Hogan.compile(gaTemplate[0]);
+                var rendered = template.render(gaData[0].metrics[0]);
+                console.log(gaData);
+                $('body').append(rendered);
+            });
+    }
+
+    function init() {
+        makeGoogle();
+    }
+
+    init();
 
 });
