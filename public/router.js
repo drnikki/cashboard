0fbf89424f4947216817e64fc00251283062d4f9
matthewlein <matthewlein@hotmail.com>
2013-11-20 11:14:24 -0600

    little cleanup and commenting

--- public/router.js
+++ public/router.js
@@ -1,11 +1,18 @@
+// ************************************************************************* //
+// ========================================================================= //
+//
+// BACKBONE Router
+//
+// ========================================================================= //
+// ************************************************************************* //
+
+
 define(function(require) {
 
     // External dependencies.
     var Backbone = require("backbone");
     var $ = require('jquery');
 
-    var FooModel = require('modules/models/foo');
-    var FooView = require('modules/views/foo');
     var FooCollection = require('modules/collections/foo');
     var FooCollectionView = require('modules/views/fooCollection');
 
@@ -19,17 +26,17 @@ define(function(require) {
         index: function() {
 
             $.ajax({
-                url: '/data/foos',
-                data : 'JSON'
+                url: '/data/foos'
             })
             .done(function(data) {
 
+                // populate the collection with the JSON
                 var fooCollection = new FooCollection( data );
-
+                // make a new view with the collection
                 var view = new FooCollectionView({ collection: fooCollection });
-
                 var viewEl = view.render().el;
 
+                // add the collection element to the body
                 $('body').append(viewEl);
 
             });
