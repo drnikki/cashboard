34c1237fba7abb5f48191492c6a29f00fc546e28
rachelfairbanks <rfairbanks.1102@gmail.com>
2013-12-02 15:48:00 -0500

    initial commit

--- app.js
+++ app.js
@@ -5,9 +5,9 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var dataRoutes = require('./routes/data');
 
 var app = express();
 
@@ -29,7 +29,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get( '/data/:provider', dataRoutes.dataRouter );
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
