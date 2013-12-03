c3114a7e670604e5b5d0522cdf715b9f601a18bf
rachelfairbanks <rfairbanks.1102@gmail.com>
2013-12-03 11:26:55 -0500

    temp GA commit

--- routes/data.js
+++ routes/data.js
@@ -4,4 +4,23 @@ var dataRoutes = {};
 // Google Analytics
 // ------------------------------------------------------------------------- //
 
-var GA = require('googleanalytics');
+var GA = require('googleanalytics'),
+    util = require('util'),
+    config = {
+        "user": "myusername",
+        "password": "mypassword"
+    },
+    ga = new GA.GA(config);
+
+     var options = {
+    'ids': 'ga:<profileid>',
+    'start-date': '2010-09-01',
+    'end-date': '2010-09-30',
+    'dimensions': 'ga:pagePath',
+    'metrics': 'ga:pageviews',
+    'sort': '-ga:pagePath'
+};
+
+ga.get(options, function(err, entries) {
+    util.debug(JSON.stringify(entries));
+});
