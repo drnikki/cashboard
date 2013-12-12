3f054edc7fec587e2923b8de59e0a9d64845593c
matthewlein <matthewlein@hotmail.com>
2013-12-12 12:07:33 -0600

    instagram and twitter pull from mongo now. They are the easy ones

--- routes/data.js
+++ routes/data.js
@@ -134,76 +134,21 @@ dataRoutes.ga = function(req, res){
         res.json(output);
     });
 
-
-    /*
-
-    // make a new google connection with this user/pass
-    var ga = new GA.GA({
-        'user' : config.google_analytics.user,
-        'password' : config.google_analytics.password
-    });
-
-    // login
-    ga.login(function(err, token) {
-
-        // metrics and dimensions reference
-        // https://developers.google.com/analytics/devguides/reporting/core/dimsmets
-
-        // metrics you want from google
-        var metrics = [
-            'ga:visitors',
-            'ga:percentNewVisits'
-        ];
-        // dimension you want from google
-        var dimensions = [
-        ];
-
-        var options = {
-            'ids': 'ga:77232304',
-            'start-date': '2013-10-03',
-            'end-date': '2013-10-03',
-            //'dimensions': dimensions.join(','),
-            'metrics': metrics.join(','),
-            //'sort': '-ga:visitCount'
-        };
-
-        // get these
-        ga.get(options, function(err, entries) {
-
-            // return JSON
-            // also its an array and I don't want that, so [0]
-            res.json({
-                // status for not found?
-                "statusCode" : 200,
-                "data" : entries[0]
-            });
-        });
-    });
-
-    */
-
 };
 
 // ------------------------------------------------------------------------- //
 // Instagram
 // ------------------------------------------------------------------------- //
 
-var Instagram = require('instagram-node-lib');
-
-Instagram.set('client_id', config.instagram.client_id);
-Instagram.set('client_secret', config.instagram.client_secret);
-
 dataRoutes.instagram = function(req, res){
 
-    Instagram.users.info({
-        user_id: config.instagram.user_id,
-        complete : function(data) {
-            res.json({
-                "statusCode" : 200,
-                "data" : data
-            });
-        }
-    });
+    db.instagram.find({}, function(err, docs){
+            // limit of 7 set below
+            console.log(docs);
+            res.json(docs);
+        })
+        // limit to 7 returned docs
+        .limit(7);
 
 };
 
@@ -211,28 +156,15 @@ dataRoutes.instagram = function(req, res){
 // Twitter
 // ------------------------------------------------------------------------- //
 
-var Twitter = require('twitter');
-var twit = new Twitter({
-    consumer_key: config.twitter.consumer_key,
-    consumer_secret: config.twitter.consumer_secret,
-    access_token_key: config.twitter.access_token,
-    access_token_secret: config.twitter.access_token_secret
-});
-
 dataRoutes.twitter = function(req, res) {
 
-    twit
-        .verifyCredentials(function(data) {
-            res.json({
-                "statusCode" : 200,
-                "data" : data
-            });
-            /*
-            twit.get('/followers/ids.json?user_id=' + config.twitter.user_id, {include_entities:true}, function(data) {
-                res.json(data);
-            });
-            */
-        });
+    db.twitter.find({}, function(err, docs){
+            // limit of 7 set below
+            console.log(docs);
+            res.json(docs);
+        })
+        // limit to 7 returned docs
+        .limit(7);
 
 };
 
